78b550e6aeeccba1f74474d5d63abecd7aa310a2
Flávio Mendes <flaviojmendes@gmail.com>
2017-01-30 08:48:35 -0200

    added dynamic map

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 $(function(){
 
+  level = [];
+  index = [];
+
     var api = "https://api.import.io/store/connector/77901d68-0a77-41e2-8958-58b6256ed6fd/_query?input=webpage/url:http%3A%2F%2Fwww.adasa.df.gov.br%2Fmonitoramento%2Fniveis-de-reservatorios%2Fhistorico&&_apikey=b6da5c753cc645d29fe8ef428357e9820e923ce8c5a3b5f2644290e6ba3a89490edb7d1f53d728d887afff93e56927f11b0cdcc44dfa9fc776e0c5f21ce07ad4b43bf03aa838da1793c86893a399c674";
     $.getJSON( api, {
         tags: "mount rainier",
@@ -14,10 +17,33 @@ $(function(){
               }, 1000);
               $('#qty').text(item.volumetil_number+"%");
               $('.date').html(item.data_value + " às " + item.horrioda_value);
-            } else {
-              return true;
+
+            }
+            if(i % 2 == 0) {
+              level[i] = item.volumetil_number;
+              index[i] = i;
             }
           });
+
+          new Chartist.Line('#levelChart', {
+            labels: index,
+            series: [
+              level
+            ]
+          }, {
+            low: 0,
+            high: 100,
+            onlyInteger: false,
+            showArea: true,
+            showLine: false,
+            showPoint: false,
+            fullWidth: true,
+            axisX: {
+              showLabel: false,
+              showGrid: false
+            }
+          });
+
         });
 
 
